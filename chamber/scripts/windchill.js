e58f91fa9aeb97aaea486d9f80e092ab509d950b
lagoke <72529997+lagoke@users.noreply.github.com>
2023-06-30 20:00:22 -0400

    Complete chamber project

--- chamber/scripts/windchill.js
+++ chamber/scripts/windchill.js
@@ -1,6 +1,7 @@
 
 const url = "https://api.openweathermap.org/data/2.5/weather?q=Atlanta,US&appid=4009c74d236b511eec4482ecef547e48&units=imperial";
 
+
 let WeatherIcon = document.querySelector('#weather-icon');
 let CurrentTemp = document.querySelector('#temp-value');
 let WindSpeedKm = document.querySelector('#wind-speed-value');
@@ -15,6 +16,7 @@ async function apiFetch() {
     if (response.ok) {
       const data = await response.json();
       //console.log(data);
+
       displayResults(data);
     } else {
       throw Error(await response.text());
@@ -36,8 +38,8 @@ function displayResults(weatherData) {
 
 
   const windSpeedKm = weatherData.wind.speed * 1.609;
-
-  CurrentTemp.innerHTML = `<strong>${weatherData.main.temp.toFixed(0)}</strong>`;
+  
+  CurrentTemp.innerHTML = `<strong>${weatherData.main.temp.toFixed(0)}</strong>`; //* Retrieves current temperature data from the weather API
 
   const iconsrc = `https://openweathermap.org/img/w/${weatherData.weather[0].icon}.png`;
   const desc = weatherData.weather[0].description;
@@ -48,11 +50,10 @@ function displayResults(weatherData) {
 
   WindSpeedKm.innerHTML = `${windSpeedKm.toFixed(2)}`;
 
-  if (windSpeedKm < 3 || weatherData.main.temp < 20) {
-   
+  if (weatherData.wind.speed < 3 || weatherData.main.temp < 50) {
     WindChillDisplay.textContent = "N/A";
   } else {
-    let windChill = 35.74 + 0.6215 * weatherData.main.temp - 35.75 * Math.pow(windSpeedKm, 0.16) + 0.4275 * weatherData.main.temp * Math.pow(windSpeedKm, 0.16);
+    let windChill = 35.74 + 0.6215 * weatherData.main.temp - 35.75 * Math.pow(weatherData.wind.speed, 0.16) + 0.4275 * weatherData.main.temp * Math.pow(weatherData.wind.speed, 0.16);
     WindChillDisplay.textContent = windChill.toFixed(2) + "°F";
   }
 }
